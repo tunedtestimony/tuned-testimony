927eb65e3308ca2b445978e408f57f1fcdcbd1cf
David Martin <contactsuperdave@gmail.com>
2026-09-02 00:58:34 -0600

    Adding latest international release

--- src/data/songs/international/singles.ts
+++ src/data/songs/international/singles.ts
@@ -2,178 +2,112 @@ import type { Song } from "../types";
 
 export const internationalSingles: Song[] = [
   {
-    slug: "ya-regocijemos",
+    slug: "now-let-us-rejoice-spanish",
     title: "Ya regocijemos",
     collection: "International",
     releaseType: "Single",
     style: "Spanish",
     releaseDate: "2026-05-29",
-    image: "/songs/ya-regocijemos.jpg",
+    image: "/songs/hymns/international/now-let-us-rejoice-spanish.png",
     description: "A Spanish-language arrangement of the beloved hymn Now Let Us Rejoice.",
     hyperFollow: "https://distrokid.com/hyperfollow/tunedtestimony/ya-regocijemos-now-let-us-rejoice---spanish-2",
     links: [
-      {
-        name: "Spotify",
-        url: "https://open.spotify.com/album/7GP5IyiJazqIIEFES8yY5F?si=TyIKroI0Sq693HMX5Cnutw",
-      },
-      {
-        name: "YouTube Music",
-        url: "https://music.youtube.com/watch?v=LIq43ufveGU&si=dI29R3ZYT3yDjsEk",
-      },
-      {
-        name: "Apple Music",
-        url: "https://music.apple.com/us/album/ya-regocijemos-now-let-us-rejoice-spanish-single/6775444653",
-      },
-      {
-        name: "Amazon Music",
-        url: "https://music.amazon.com/tracks/B0H2YF79RK?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_jnOqs47JiNdj88XGJiyB4JRUN",
-      },
-      {
-        name: "Lyric Video",
-        url: "https://youtu.be/IplyqcYfxY4",
-      },
+      { name: "Spotify", url: "https://open.spotify.com/album/7GP5IyiJazqIIEFES8yY5F?si=TyIKroI0Sq693HMX5Cnutw", },
+      { name: "YouTube Music", url: "https://music.youtube.com/watch?v=LIq43ufveGU&si=dI29R3ZYT3yDjsEk", },
+      { name: "Apple Music", url: "https://music.apple.com/us/album/ya-regocijemos-now-let-us-rejoice-spanish-single/6775444653", },
+      { name: "Amazon Music", url: "https://music.amazon.com/tracks/B0H2YF79RK?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_jnOqs47JiNdj88XGJiyB4JRUN", },
+      { name: "Lyric Video", url: "https://youtu.be/IplyqcYfxY4", },
     ],
   },
   {
-    slug: "alegres-cantemos",
+    slug: "now-let-us-rejoice-portuguese",
     title: "Alegres Cantemos",
     collection: "International",
     releaseType: "Single",
     style: "Portuguese",
     releaseDate: "2026-06-05",
-    image: "/songs/alegres-cantemos.jpg",
-    description:
-      "A Portuguese-language arrangement of the beloved hymn Now Let Us Rejoice.",
-    hyperFollow:
-      "https://distrokid.com/hyperfollow/tunedtestimony/alegres-cantemos-now-let-us-rejoice---portuguese-2",
+    image: "/songs/hymns/international/now-let-us-rejoice-portuguese.png",
+    description: "A Portuguese-language arrangement of the beloved hymn Now Let Us Rejoice.",
+    hyperFollow: "https://distrokid.com/hyperfollow/tunedtestimony/alegres-cantemos-now-let-us-rejoice---portuguese-2",
     links: [
-      {
-        name: "Spotify",
-        url: "https://open.spotify.com/track/6HRqv1XHKer3jVAG6vqMOL?si=533677cb1a78402d",
-      },
-      {
-        name: "YouTube Music",
-        url: "https://music.youtube.com/playlist?list=OLAK5uy_mdNuSi1EBk779dYL4TZ9HOmrS6WlJclLA&si=HArpeZK1lAlgYh8R",
-      },
-      {
-        name: "Apple Music",
-        url: "https://music.apple.com/us/song/alegres-cantemos-now-let-us-rejoice-portuguese/6773519338",
-      },
-      {
-        name: "Amazon Music",
-        url: "https://music.amazon.com/albums/B0H31W2JSB?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_LAGBRB2jAyoKPac8I4uUUtyau&trackAsin=B0H31WG8LH",
-      },
-      {
-        name: "Lyric Video",
-        url: "https://youtu.be/qfMw80tIdOI",
-      },
+      { name: "Spotify", url: "https://open.spotify.com/track/6HRqv1XHKer3jVAG6vqMOL?si=533677cb1a78402d", },
+      { name: "YouTube Music", url: "https://music.youtube.com/playlist?list=OLAK5uy_mdNuSi1EBk779dYL4TZ9HOmrS6WlJclLA&si=HArpeZK1lAlgYh8R", },
+      { name: "Apple Music", url: "https://music.apple.com/us/song/alegres-cantemos-now-let-us-rejoice-portuguese/6773519338", },
+      { name: "Amazon Music", url: "https://music.amazon.com/albums/B0H31W2JSB?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_LAGBRB2jAyoKPac8I4uUUtyau&trackAsin=B0H31WG8LH", },
+      { name: "Lyric Video", url: "https://youtu.be/qfMw80tIdOI", },
     ],
   },
   {
-    slug: "tayoy-magalak",
+    slug: "now-let-us-rejoice-tagalog",
     title: "Tayo’y Magalak",
     collection: "International",
     releaseType: "Single",
     style: "Tagalog",
     releaseDate: "2026-06-12",
-    image: "/songs/tayoy-magalak.jpg",
-    description:
-      "A Tagalog-language arrangement of the beloved hymn Now Let Us Rejoice.",
-    hyperFollow:
-      "https://distrokid.com/hyperfollow/tunedtestimony/tayoy-magalak-now-let-us-rejoice---tagalog",
+    image: "/songs/hymns/international/now-let-us-rejoice-tagalog.png",
+    description: "A Tagalog-language arrangement of the beloved hymn Now Let Us Rejoice.",
+    hyperFollow: "https://distrokid.com/hyperfollow/tunedtestimony/tayoy-magalak-now-let-us-rejoice---tagalog",
     links: [
-      {
-        name: "Spotify",
-        url: "https://open.spotify.com/album/5Kac3JzuDgwUKLRpDkcqQA?si=X1ycmeqZTMuLbF-mDrngnQ",
-      },
-      {
-        name: "YouTube Music",
-        url: "https://music.youtube.com/playlist?list=OLAK5uy_mTJNcUaYYmXrBNVsyjS6e0NbDfpehLzC0&si=xR7Ssu4aj3Y53Ud2",
-      },
-      {
-        name: "Apple Music",
-        url: "https://music.apple.com/us/song/tayoy-magalak-now-let-us-rejoice-tagalog/6773226878",
-      },
-      {
-        name: "Amazon Music",
-        url: "https://music.amazon.com/albums/B0H2YLBT5J?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_lYbpfVASi94Gp1Mo1GfdtXMKo&trackAsin=B0H2YD56G6",
-      },
-      {
-        name: "Lyric Video",
-        url: "https://youtu.be/6ZMAOr7F7y4",
-      },
+      { name: "Spotify", url: "https://open.spotify.com/album/5Kac3JzuDgwUKLRpDkcqQA?si=X1ycmeqZTMuLbF-mDrngnQ", },
+      { name: "YouTube Music", url: "https://music.youtube.com/playlist?list=OLAK5uy_mTJNcUaYYmXrBNVsyjS6e0NbDfpehLzC0&si=xR7Ssu4aj3Y53Ud2", },
+      { name: "Apple Music", url: "https://music.apple.com/us/song/tayoy-magalak-now-let-us-rejoice-tagalog/6773226878", },
+      { name: "Amazon Music", url: "https://music.amazon.com/albums/B0H2YLBT5J?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_lYbpfVASi94Gp1Mo1GfdtXMKo&trackAsin=B0H2YD56G6", },
+      { name: "Lyric Video", url: "https://youtu.be/6ZMAOr7F7y4", },
     ],
   },
 
 	{
-		slug: "ich-weiss-dass-mein-erloeser-lebt",
+		slug: "i-know-that-my-redeemer-lives-german",
 		title: "Ich weiß, daß mein Erlöser lebt",
 		collection: "International",
 		releaseType: "Single",
 		style: "German",
 		releaseDate: "2026-08-04",
-		image: "/songs/ich-weiss-dass-mein-erloeser-lebt.jpg",
-		description:
-			"A German-language arrangement of the beloved hymn I Know That My Redeemer Lives.",
-		hyperFollow:
-			"https://distrokid.com/hyperfollow/tunedtestimony/ich-wei-da-mein-erlser-lebt-i-know-that-my-redeemer-lives---german-2",
+		image: "/songs/hymns/international/i-know-that-my-redeemer-lives-german.png",
+		description: "A German-language arrangement of the beloved hymn I Know That My Redeemer Lives.",
+		hyperFollow: "https://distrokid.com/hyperfollow/tunedtestimony/ich-wei-da-mein-erlser-lebt-i-know-that-my-redeemer-lives---german-2",
 		links: [
-			{
-				name: "Spotify",
-				url: "https://open.spotify.com/album/0LwdsIhoWyNruheTUKRhtY",
-			},
-			{
-				name: "YouTube Music",
-				url: "https://music.youtube.com/watch?v=BcNQvj2IB6w&si=k3j7yOStSRPM28Ac",
-			},
-			{
-				name: "Apple Music",
-				url: "https://music.apple.com/us/album/ich-wei%C3%9F-da%C3%9F-mein-erl%C3%B6ser-lebt-i-know-that-my-redeemer/6776904475?uo=4",
-			},
-			{
-				name: "Amazon Music",
-				url: "https://music.amazon.com/albums/B0H44HFR9V?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_LwJOoD7l4FKzk0MA7IP54tEh1&trackAsin=B0H44L4D5L",
-			},
-			{
-				name: "Lyric Video",
-				url: "https://youtu.be/hbK-sITctnE",
-			},
+			{ name: "Spotify", url: "https://open.spotify.com/album/0LwdsIhoWyNruheTUKRhtY", },
+			{ name: "YouTube Music", url: "https://music.youtube.com/watch?v=BcNQvj2IB6w&si=k3j7yOStSRPM28Ac", },
+			{ name: "Apple Music", url: "https://music.apple.com/us/album/ich-wei%C3%9F-da%C3%9F-mein-erl%C3%B6ser-lebt-i-know-that-my-redeemer/6776904475?uo=4", },
+			{ name: "Amazon Music", url: "https://music.amazon.com/albums/B0H44HFR9V?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_LwJOoD7l4FKzk0MA7IP54tEh1&trackAsin=B0H44L4D5L", },
+			{ name: "Lyric Video", url: "https://youtu.be/hbK-sITctnE", },
 		],
 	},
 	{
-		slug: "jesus-minha-luz",
+		slug: "the-lord-is-my-light-portuguese",
 		title: "Jesus, Minha Luz",
 		collection: "International",
 		releaseType: "Single",
 		style: "Portuguese",
 		releaseDate: "2025-10-02",
-		image: "/songs/jesus-minha-luz.png",
-		description:
-			"A Portuguese-language arrangement of the beloved hymn The Lord Is My Light.",
-		hyperFollow:
-			"https://distrokid.com/hyperfollow/hinoderiva/jesus-minha-luz-pop-ballad-version",
+		image: "/songs/hymns/international/the-lord-is-my-light-portuguese.png",
+		description: "A Portuguese-language arrangement of the beloved hymn The Lord Is My Light.",
+		hyperFollow: "https://distrokid.com/hyperfollow/hinoderiva/jesus-minha-luz-pop-ballad-version",
 		links: [
-			{
-				name: "Spotify",
-				url: "https://open.spotify.com/album/3Pvk41eTHjyxH5XJGoSNER",
-			},
-			{
-				name: "YouTube Music",
-				url: "https://music.youtube.com/watch?v=3Wp9iQArvag&si=amTog9FIeEPEzE3t",
-			},
-			{
-				name: "Apple Music",
-				url: "https://music.apple.com/us/album/jesus-minha-luz-pop-ballad-version-single/1844107864?uo=4",
-			},
-			{
-				name: "Amazon Music",
-				url: "https://music.amazon.com/tracks/B0FTMKTYD9?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_J60TB5C0IF1fwSREa4KGLLJTY",
-			},
-			{
-				name: "Lyric Video",
-				url: "https://youtu.be/3Wp9iQArvag",
-			},
+			{ name: "Spotify", url: "https://open.spotify.com/album/3Pvk41eTHjyxH5XJGoSNER", },
+			{ name: "YouTube Music", url: "https://music.youtube.com/watch?v=3Wp9iQArvag&si=amTog9FIeEPEzE3t", },
+			{ name: "Apple Music", url: "https://music.apple.com/us/album/jesus-minha-luz-pop-ballad-version-single/1844107864?uo=4", },
+			{ name: "Amazon Music", url: "https://music.amazon.com/tracks/B0FTMKTYD9?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_J60TB5C0IF1fwSREa4KGLLJTY", },
+			{ name: "Lyric Video", url: "https://youtu.be/3Wp9iQArvag", },
 		],
 	},
-
+	{
+		slug: "i-need-thee-every-hour-japanese",
+		title: "絶えず頼り主求む",
+		collection: "International",
+		releaseType: "Single",
+		style: "Japanese",
+		releaseDate: "2025-10-02",
+		image: "/songs/hymns/international/i-need-thee-every-hour-japanese.png",
+		description: "A Japanese-language arrangement of the beloved hymn I Need Thee Every Hour.",
+		hyperFollow: "https://distrokid.com/hyperfollow/tunedtestimony/-i-need-thee-every-hour---japanese",
+		links: [
+			{ name: "Spotify", url: "https://open.spotify.com/track/3Oc3LtDVQAv9eoCGuCeC2h?si=1f46a1ab7b8a48d9", },
+			{ name: "YouTube Music", url: "https://music.youtube.com/watch?v=As1Q69jB3gE&si=v5K44NKlvjHOZNgu", },
+			{ name: "Apple Music", url: "https://music.apple.com/us/album/jesus-minha-luz-pop-ballad-version-single/1844107864?uo=4", },
+			{ name: "Amazon Music", url: "http://www.amazon.com/gp/product/B0H48VNKMK", },
+			{ name: "Lyric Video", url: "https://youtube.com/shorts/E8ZB-jMwwCo?feature=share", },
+		],
+	},
 ];
